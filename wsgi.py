80aa996ce25d5eb24898bb3c54c95f049a7878a7
Souparno Goswami <165120778+Souparno467@users.noreply.github.com>
2026-02-12 19:27:01 +0530

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-"""
-WSGI entrypoint for production servers (e.g. Waitress/Gunicorn).
-
-Exposes `application` which wraps the Flask `app` defined in app.py.
-"""
-
-from app import app as application
-
-
